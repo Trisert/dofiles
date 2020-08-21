20c8abea571e87f1302c26d9d302859377cc373b
Trisert <36551466+Trisert@users.noreply.github.com>
2020-08-21 20:42:35 +0200

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,124 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  system.autoUpgrade.enable= true;
+  system.autoUpgrade.allowReboot = true;
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+   networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.eno1.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+   i18n.defaultLocale = "it_IT.UTF-8";
+   console = {
+     font = "Lat2-Terminus16";
+     keyMap = "it";
+   };
+
+  # Set your time zone.
+   time.timeZone = "Europe/Rome";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+   environment.systemPackages = with pkgs; [
+     wget
+     xclip 
+     vim 
+     git
+     firefox 
+     xmonad-with-packages 
+     netbeans 
+     libreoffice-fresh
+   ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  #   pinentryFlavor = "gnome3";
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+  
+   services = {
+  # Enable CUPS to print documents.
+         printing.enable = true;
+  # Enable Tor
+         tor.enable = false;
+    };
+
+  # Enable sound.
+   sound.enable = true;
+   hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+   services.xserver.enable = true;
+   services.xserver.layout = "it";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+   services.xserver.displayManager.sddm.enable = true;
+   services.xserver.desktopManager.plasma5.enable = true;
+   services.xserver.windowManager = {                     # Open configuration for the window manager.
+    xmonad.enable = true;                                # Enable xmonad.
+    xmonad.enableContribAndExtras = true;                # Enable xmonad contrib and extras.
+    xmonad.extraPackages = hpkgs: [                      # Open configuration for additional Haskell packages.
+      hpkgs.xmonad-contrib                               # Install xmonad-contrib.
+      hpkgs.xmonad-extras                                # Install xmonad-extras.
+      hpkgs.xmonad                                       # Install xmonad itself.
+    ];
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.users.nicola = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+   };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+}
